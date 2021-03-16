5e8dc58f1c24f4d6c9d34c973244b52f87599019
zhuo <joelee_mail@163.com>
2021-03-16 17:13:05 +0800

    [mod] some bug fixed

---
+++ runit-action/runit-action-screencapture/src/main/java/com/fakeoder/runit/action/screencapture/ScreenCapture.java
@@ -0,0 +1,15 @@
+package com.fakeoder.runit.action.screencapture;
+
+import com.fakeoder.runit.core.action.Action;
+import com.fakeoder.runit.core.action.ActionResult;
+
+/**
+ * @author zhuo
+ */
+public class ScreenCapture extends Action {
+
+    @Override
+    public ActionResult run() {
+        return super.run();
+    }
+}
---
+++ runit-web/src/main/java/com/fakeoder/runit/web/RunitApplication.java
@@ -0,0 +1,14 @@
+package com.fakeoder.runit.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhuo
+ */
+@SpringBootApplication
+public class RunitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunitApplication.class,args);
+    }
+}
